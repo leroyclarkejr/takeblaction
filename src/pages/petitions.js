a68b4ac051695677ba724ef98e9312ca150dc55b
Leroy Clarke Jr <lclarkejr94@gmail.com>
2020-06-08 02:01:43 -0400

    fix

--- src/pages/petitions.js
+++ src/pages/petitions.js
@@ -4,6 +4,8 @@ import openlink from "../images/openlink.svg"
 import LaunchIcon from "@material-ui/icons/Launch"
 // import Button from "@material-ui/core/Button"
 
+const url =
+  "https://script.googleusercontent.com/macros/echo?user_content_key=mYmgHnlrxwR9YtCB9E_QyG8C7RotfpQmT4Fw9Tn3YA1fTWzgyrNCLloRe6_JOana8EVElZx0Lg_MPfZ-oKRLgo3NlhxxzX_ym5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDSs5sRgD1ac-h3oV3AasSem-5e7OFQzXI5YTTJP454RdAgflvzraAzfveALHdp65b3s74y5e3r9&lib=MpXQzipjhDujhrJj9-lpphnKgdv5WXFdI"
 class Petitions extends React.Component {
   constructor(props) {
     super(props)
@@ -13,15 +15,16 @@ class Petitions extends React.Component {
   }
 
   componentDidMount() {
-    fetch(
-      "https://script.googleusercontent.com/macros/echo?user_content_key=mYmgHnlrxwR9YtCB9E_QyG8C7RotfpQmT4Fw9Tn3YA1fTWzgyrNCLloRe6_JOana8EVElZx0Lg_MPfZ-oKRLgo3NlhxxzX_ym5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDSs5sRgD1ac-h3oV3AasSem-5e7OFQzXI5YTTJP454RdAgflvzraAzfveALHdp65b3s74y5e3r9&lib=MpXQzipjhDujhrJj9-lpphnKgdv5WXFdI"
-    )
+    const [output, setOutput] = useState(0)
+
+    fetch(url)
       .then(res => {
+        console.log(res)
         return res.json()
       })
       .then(data => {
+        console.log(data)
         let petition = data.petitions.map(value => {
-          console.log(value)
           return (
             <div
               className="data"
