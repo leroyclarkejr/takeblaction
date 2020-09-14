85ea876cbcbb8a1ca6c0ffaae9b9e492ba4699f2
Leroy Clarke Jr <lclarkejr94@gmail.com>
2020-09-14 14:30:35 -0400

    New Favicon added

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,13 +34,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Take Blaction`,
+        short_name: `TB`,
         start_url: `/`,
         background_color: `#141414`,
-        theme_color: `#663399`,
+        theme_color: `#141414`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/TB-LOGO-WHITE-.svg`, // This path is relative to the root of the site.
       },
     },
 
