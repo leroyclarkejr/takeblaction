9e857c9ccbae638d10fe240474678ecdd6db5992
Leroy Clarke Jr <lclarkejr94@gmail.com>
2021-04-24 15:06:50 -0400

    Added analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,11 +44,11 @@ module.exports = {
       },
     },
 
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: "UA-167200825-2",
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-169323789-1",
+      },
+    },
   ],
 }
